d97030f4d3644ccf1bbdcaac8bef22709fd4abbd
Nicolas Bouchard <nicolas.bouchard@gmail.com>
2018-06-22 12:59:25 -0400

    Fixed permission, added svg.js, change text on rect

--- public/js/SVGHandling.js
+++ public/js/SVGHandling.js
@@ -2,7 +2,13 @@ var SVGMap = {
     doc: false,
     init: function(e) {
         this.doc = e.target.ownerDocument;
-        
+
         console.log("Init has been called!");
+
+        var element = SVG.get('txt30002131');
+        //element.rotate(45);
+        element.node.textContent = 'test';
+        console.log(element);
+
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,6 +12,7 @@
 
         <script async src="{{ URL::asset('js/SVGHandling.js') }}"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="{{ URL::asset('js/svg.min.js') }}"></script>
 
         <script type="text/javascript">
             $(document).ready(function(){
